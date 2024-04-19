88622289c846d3c72d0c587721d411cdfe3f40f5
Juliansv <juliansv22@gmail.com>
2024-04-19 21:23:18 +0200

    add types and fix minor issues

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { getImagesByCollection } from "@/actions/get-images";
 import Image from "next/image";
 import {
   Carousel,
@@ -7,43 +6,90 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { getCollection } from "../lib/get-collections";
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const name = decodeURIComponent(params.slug.replace(/-/g, "%20"));
-  const images = await getImagesByCollection(name);
+interface Collection {
+  title?: String;
+  description?: String;
+  coverPhoto?: {
+    sys: {
+      id: String;
+    };
+    fields: {
+      title: String;
+      file: {
+        url: String;
+      };
+    };
+  };
+  photos?: Array<{
+    sys: {
+      id: String;
+    };
+    fields: {
+      title: String;
+      file: {
+        url: String;
+      };
+    };
+  }>;
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { id: string };
+}) {
+  const collection: Collection = await getCollection(searchParams.id);
+
+  const title = collection.title?.toString() || "Undefined Title";
+
+  const images = Array.from(collection.photos || []);
+
   return (
-    <div className="md:flex md:flex-col md:h-full">
-      <h1 className="p-6 text-center text-3xl font-extrabold">{name}</h1>
-      <div className="columns-2 gap-2 space-y-2 px-2 !relative md:hidden">
+    <div className="md:flex md:h-full md:flex-col">
+      <h1 className="p-6 text-center text-3xl font-extrabold">{title}</h1>
+      <div className="!relative columns-1 gap-2 space-y-2 px-2 sm:columns-2 md:hidden">
         {images.map((image) => (
           <Image
-            key={image.id}
-            src={image.download_url}
-            alt={image.author}
+            key={image.sys.id.toString()}
+            src={"https:" + image.fields.file.url.toString()}
+            alt={image.fields.title.toString()}
             fill={true}
             sizes="(max-width: 768px) 33vw, (max-width: 1200px) 50vw, 100vw"
             className="!relative"
           />
         ))}
       </div>
-      <div className="max-h-full overflow-hidden flex flex-1 mb-1">
-        <Carousel id="Carousel-parent" className="hidden md:flex md:flex-1 static justify-center">
+      <div className="mb-1 flex max-h-full flex-1 overflow-hidden">
+        <Carousel
+          id="Carousel-parent"
+          className="static hidden justify-center md:flex md:flex-1"
+        >
           <CarouselContent id="carousel-content" className="m-0 h-full">
             {Array.from(images).map((image) => (
-              <CarouselItem key={Math.random()} id="Carousel-item" className="p-0 relative flex justify-center overflow-hidden">
-                  <Image
-                    src={image.download_url}
-                    alt={image.author}
-                    height={500}
-                    width={500}
-                    className="w-auto h-auto"
-                    id="Carousel-Image"
-                  />
+              <CarouselItem
+                key={Math.random()}
+                id="Carousel-item"
+                className="relative flex justify-center overflow-hidden p-0"
+              >
+                <Image
+                  src={"https:" + image.fields.file.url.toString()}
+                  alt={image.fields.title.toString()}
+                  height={500}
+                  width={500}
+                  className="h-auto w-auto"
+                  id="Carousel-Image"
+                />
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="absolute left-8 scale-125 bg-transparent">Previous</CarouselPrevious>
-          <CarouselNext className="absolute right-8 scale-125 bg-transparent">Next</CarouselNext>
+          <CarouselPrevious className="absolute left-8 scale-125 bg-transparent">
+            Previous
+          </CarouselPrevious>
+          <CarouselNext className="absolute right-8 scale-125 bg-transparent">
+            Next
+          </CarouselNext>
         </Carousel>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import { getCollections } from "@/actions/get-images";
 import Image from "next/image";
 import Link from "next/link";
-import getCollectionsNew from "./lib/get-collections";
+import { getCollections } from "./lib/get-collections";
 import {
   Carousel,
   CarouselContent,
@@ -10,62 +9,100 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+interface Collection {
+  sys: {
+    id: string;
+  };
+  fields: {
+    title?: string;
+    description?: string;
+    coverPhoto?: {
+      fields: {
+        file: {
+          url: string;
+        };
+      };
+    };
+  };
+}
+
 export default async function Home() {
-  const collections = await getCollections();
-  const newCollections = await getCollectionsNew();
-  // console.log(newCollections);
+  const collections: Collection[] = await getCollections();
+
+  console.log(collections);
+
+  const defaultAvatar =
+    "//png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg";
 
   return (
     <>
       <div className="grid w-full gap-2 md:hidden">
-        {Array.from(collections).map(([author, image]) => (
+        {Array.from(collections).map((collection) => (
           <Link
-            href={`/${author}`}
-            key={Math.random()}
+            href={{
+              pathname: `/${collection.fields.title}`,
+              query: {
+                id: collection.sys.id,
+              }
+            }}
+            key={collection.sys.id}
             className="relative hover:text-slate-300"
           >
             <Image
-              src={image[0]}
-              alt={author}
+              src={
+                "https:" +
+                `${collection.fields.coverPhoto ? collection.fields.coverPhoto.fields.file.url : defaultAvatar}`
+              }
+              alt={collection.sys.id}
               width={800}
               height={100}
               className="max-h-52 w-full object-cover opacity-80"
             />
             <p className="absolute top-[45%] w-full text-center text-3xl font-semibold">
-              {author}
+              {collection.fields.title}
             </p>
           </Link>
         ))}
       </div>
       <Carousel className="hidden md:block md:h-full">
         <CarouselContent className="h-full">
-          {Array.from(collections).map(([author, image]) => (
+          {Array.from(collections).map((collection) => (
             <CarouselItem key={Math.random()}>
               <Link
-                href={`/${author}`}
-                className="relative flex h-full hover:text-slate-300 hover:scale-105 transition-all ease-in-out duration-300"
+                href={{
+                  pathname: `/${collection.fields.title}`,
+                  query: {
+                    id: collection.sys.id,
+                  }
+                }}
+                className="relative flex h-full transition-all duration-300 ease-in-out hover:scale-105 hover:text-slate-300"
               >
                 <Image
-                  src={image[0]}
-                  alt={author}
+                  src={
+                    "https:" +
+                    `${collection.fields.coverPhoto ? collection.fields.coverPhoto.fields.file.url : defaultAvatar}`
+                  }
+                  alt={collection.sys.id}
                   height={800}
                   width={800}
-                  className="object-cover w-full opacity-80"
+                  className="w-full object-cover opacity-80"
                 />
                 <div className="absolute top-[50%] w-full text-center">
                   <p className=" text-3xl font-semibold">
-                    {author}
+                    {collection.fields.title}
                   </p>
-                  <p>
-                    Descripcion de prueba
-                  </p>
+                  <p>{collection.fields.description}</p>
                 </div>
               </Link>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="absolute left-8 scale-125 bg-transparent">Previous</CarouselPrevious>
-        <CarouselNext className="absolute right-8 scale-125 bg-transparent">Next</CarouselNext>
+        <CarouselPrevious className="absolute left-8 scale-125 bg-transparent">
+          Previous
+        </CarouselPrevious>
+        <CarouselNext className="absolute right-8 scale-125 bg-transparent">
+          Next
+        </CarouselNext>
       </Carousel>
     </>
   );

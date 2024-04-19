1d792b16b75f1d5e5deef26067e8a1cc3a824b99
Juliansv <juliansv22@gmail.com>
2024-04-19 21:23:51 +0200

    Add basic contact page wip

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,10 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="text-center h-full p-4">
+      <h2 className="text-2xl font-bold">Contact</h2>
+      <p className="mt-2">If you would like to contact me, please complete the form below</p>
+      
+    </div>
   )
 }
 
